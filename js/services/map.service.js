bd86d201d28aa67e8b89c9ab07e0b40f87f9657d
Yaniv Kela <kianiv94@gmail.com>
2022-12-21 15:22:08 +0200

    added Maps API key

--- js/services/map.service.js
+++ js/services/map.service.js
@@ -39,7 +39,7 @@ function panTo(lat, lng) {
 
 function _connectGoogleApi() {
     if (window.google) return Promise.resolve()
-    const API_KEY = '' //TODO: Enter your API Key
+    const API_KEY = 'AIzaSyCsUS1oaW8SreIAUyNQh7BCwsPLWc_eGfE' //TODO: Enter your API Key
     var elGoogleApi = document.createElement('script')
     elGoogleApi.src = `https://maps.googleapis.com/maps/api/js?key=${API_KEY}`
     elGoogleApi.async = true
